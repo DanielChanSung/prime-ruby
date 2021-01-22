7a864fbaa6ff9ad325bb1f71f4089611cb6a1fbe
Daniel Sung <DanielChanSung@gmail.com>
2021-01-22 13:42:56 -0500

    commit tree d404e13c476dddfcd0dd262a5d05562131276e96

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num) # 17, 31
+    if num <= 1
+       return false
+    end
+ 
+
+    (2..num-1).each do |prime|
+         if num % prime == 0
+            return false
+        
+        end
+    end
+return true
+end
+
+# review how to write the each block
+# once getting iteration, figure out what to ask it to do
